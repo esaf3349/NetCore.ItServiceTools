d8cc5cbba30096ea5880d8aac7c54551f69245f6
Esaf <esaf3349@gmail.com>
2022-11-25 11:19:16 +0300

    PlatformService: added app db context

---
+++ PlatformService/Source/PlatformService.Persistence.EntityFramework/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlatformService.Core.Entities;
+using PlatformService.Persistence.EntityFramework.EntityConfigs;
+
+namespace PlatformService.Persistence.EntityFramework
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new PlatformEntityConfig());
+
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Platform> Platforms { get; set; }
+    }
+}
