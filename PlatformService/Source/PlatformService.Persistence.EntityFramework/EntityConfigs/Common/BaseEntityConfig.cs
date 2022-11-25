5daddab68bd94547182d1273bdb36d5d5131fb89
Esaf <esaf3349@gmail.com>
2022-11-25 11:15:13 +0300

    PlatformService: added entity configs

---
+++ PlatformService/Source/PlatformService.Persistence.EntityFramework/EntityConfigs/Common/BaseEntityConfig.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PlatformService.Core.Entities.Common;
+
+namespace PlatformService.Persistence.EntityFramework.EntityConfigs.Common
+{
+    public abstract class BaseEntityConfig<T> : IEntityTypeConfiguration<T> where T : BaseEntity
+    {
+        public virtual void Configure(EntityTypeBuilder<T> builder)
+        {
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.IsDeleted).IsRequired();
+        }
+    }
+}
---
+++ PlatformService/Source/PlatformService.Persistence.EntityFramework/EntityConfigs/PlatformEntityConfig.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PlatformService.Core.Entities;
+using PlatformService.Persistence.EntityFramework.EntityConfigs.Common;
+
+namespace PlatformService.Persistence.EntityFramework.EntityConfigs
+{
+    public class PlatformEntityConfig : BaseEntityConfig<Platform>
+    {
+        public override void Configure(EntityTypeBuilder<Platform> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(c => c.Name)
+                .HasColumnType("nvarchar(100)")
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(c => c.Publisher)
+                .HasColumnType("nvarchar(100)")
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(c => c.Cost)
+                .HasColumnType("nvarchar(100)")
+                .HasMaxLength(100)
+                .IsRequired();
+        }
+    }
+}
