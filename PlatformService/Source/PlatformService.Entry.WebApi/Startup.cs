b89d49ee8cc5a55e99525c0527330eafc37e36a0
Esaf <esaf3349@gmail.com>
2022-11-30 14:20:20 +0300

    PlatformService: added docker

--- PlatformService/Source/PlatformService.Entry.WebApi/Startup.cs
+++ PlatformService/Source/PlatformService.Entry.WebApi/Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using PlatformService.Entry.WebApi.Middlewares;
-using AppProfile = PlatformService.Entry.SqlServerProfile;
+using AppProfile = PlatformService.Entry.InMemoryProfile;
 
 namespace PlatformService.Entry.WebApi
 {
