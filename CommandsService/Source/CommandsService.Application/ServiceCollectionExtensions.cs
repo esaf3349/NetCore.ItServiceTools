19de4eb3fc97b7d1d91c6326a7b59a0fd922b928
Esaf <esaf3349@gmail.com>
2022-12-05 14:44:14 +0300

    CommandsService: added app di

---
+++ CommandsService/Source/CommandsService.Application/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using CommandsService.Application.Common.PipelineBehaviors;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace CommandsService.Application
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddMediatrApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestPerformanceBehavior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestValidationBehavior<,>));
+
+            return services;
+        }
+    }
+}
