b5a6b5018e71f553b8192d74a5968386e43bd929
Esaf <esaf3349@gmail.com>
2022-12-05 08:50:56 +0300

    CommandsService: added exceptions, pipeline behaviors

---
+++ CommandsService/Source/CommandsService.Application/Common/Exceptions/ValidationException.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CommandsService.Application.Common.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public IDictionary<string, string[]> Failures { get; }
+
+        public ValidationException() : base("One or more validation failures have occurred.")
+        {
+            Failures = new Dictionary<string, string[]>();
+        }
+
+        public ValidationException(ValidationFailure[] failures) : this()
+        {
+            var propertyNames = failures
+                .Select(failure => failure.PropertyName)
+                .Distinct();
+
+            foreach (var propertyName in propertyNames)
+            {
+                var propertyFailures = failures
+                    .Where(failure => failure.PropertyName == propertyName)
+                    .Select(failure => failure.ErrorMessage)
+                    .ToArray();
+
+                Failures.Add(propertyName, propertyFailures);
+            }
+        }
+    }
+}
---
+++ CommandsService/Source/CommandsService.Application/Common/PipelineBehaviors/RequestValidationBehavior.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using MediatR;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using ValidationException = CommandsService.Application.Common.Exceptions.ValidationException;
+
+namespace CommandsService.Application.Common.PipelineBehaviors
+{
+    public class RequestValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public RequestValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var failures = _validators
+                .Select(validator => validator.Validate(request))
+                .SelectMany(result => result.Errors)
+                .Where(failure => failure != null)
+                .ToArray();
+
+            if (failures.Any())
+            {
+                throw new ValidationException(failures);
+            }
+
+            return next();
+        }
+    }
+}
