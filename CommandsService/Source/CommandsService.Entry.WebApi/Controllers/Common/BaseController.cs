48bafb7333e362b7a738a5dafd99fae9bb8dc81e
Esaf <esaf3349@gmail.com>
2022-12-07 17:05:58 +0300

    Kubernetes: added api gateway

--- CommandsService/Source/CommandsService.Entry.WebApi/Controllers/Common/BaseController.cs
+++ CommandsService/Source/CommandsService.Entry.WebApi/Controllers/Common/BaseController.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace CommandsService.Entry.WebApi.Controllers.Common
 {
-    [Route("api/[controller]")]
+    [Route("commands/api/[controller]")]
     [ApiController]
     public abstract class BaseController : ControllerBase
     {
