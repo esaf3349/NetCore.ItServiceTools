8e77e9b020699e11e9c4829cdd0b5ab74470d2c9
Esaf <esaf3349@gmail.com>
2022-12-08 15:54:31 +0300

    Kubernetes: added deploy, pvc, cluster ip, load balancer for platforms db

--- PlatformService/Source/PlatformService.Entry.WebApi/Startup.cs
+++ PlatformService/Source/PlatformService.Entry.WebApi/Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using PlatformService.Entry.WebApi.Middlewares;
-using AppProfile = PlatformService.Entry.InMemoryProfile;
+using AppProfile = PlatformService.Entry.SqlServerProfile;
 
 namespace PlatformService.Entry.WebApi
 {
